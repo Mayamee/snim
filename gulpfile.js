c59fc2f58c15cc7eb1ebd11733bb7746a9748d6a
Mayamee <ponyashcat228@gmail.com>
2021-03-16 00:41:17 +0300

    remove stupid headers

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const del = require("del");
 const jimp = require("gulp-jimp-wrapper");
 // for more features see - https://www.npmjs.com/package/jimp
 const rename = require("gulp-rename");
-<<<<<<< HEAD
 const concat = require("gulp-concat");
 
 function articles() {
@@ -13,8 +12,6 @@ function articles() {
     .pipe(dest("article/"));
 }
 
-=======
->>>>>>> 36d12c50c5b78cd3d96553b30e3253d17983ba30
 function images() {
   return src("img/src/*.{jpg,png,svg,gif,ico,webp}")
     .pipe(rename({ prefix: pref + "-" }))
@@ -28,13 +25,10 @@ function images() {
 function cleandest() {
   return del("img/dest/**/*", { force: true });
 }
-<<<<<<< HEAD
 exports.articles = articles;
 exports.images = series(cleandest, images, articles);
 exports.cleandest = cleandest;
 exports.default = series(cleandest, images, articles);
-=======
 exports.images = parallel(cleandest, images);
 exports.cleandest = cleandest;
 exports.default = parallel(cleandest, images);
->>>>>>> 36d12c50c5b78cd3d96553b30e3253d17983ba30
